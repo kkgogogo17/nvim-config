45740196d58768251d917e1019f392358d3d42b7
Eric <kang.zi@northeastern.edu>
2025-04-04 23:26:24 -0400

    adding config for gopls

---
+++ lua/plugins/lspconfig.lua
@@ -0,0 +1,71 @@
+return {
+  "neovim/nvim-lspconfig",
+  event = "LazyFile",
+  dependencies = {
+    "mason.nvim",
+    { "williamboman/mason-lspconfig.nvim", config = function() end },
+  },
+  opts = function()
+    ---@class PluginLspOpts
+    local ret = {
+      diagnostics = {
+        underline = true,
+        update_in_insert = false,
+        virtual_text = {
+          spacing = 4,
+          source = "if_many",
+          prefix = "●",
+        },
+        severity_sort = true,
+        signs = {
+          text = {
+            [vim.diagnostic.severity.ERROR] = "E",
+            [vim.diagnostic.severity.WARN] = "W",
+            [vim.diagnostic.severity.HINT] = "H",
+            [vim.diagnostic.severity.INFO] = "I",
+          },
+        },
+      },
+      inlay_hints = {
+        enabled = false, -- Disable inlay hints globally
+        exclude = { "vue" },
+      },
+      codelens = {
+        enabled = false,
+      },
+      capabilities = {
+        workspace = {
+          fileOperations = {
+            didRename = true,
+            willRename = true,
+          },
+        },
+      },
+      format = {
+        formatting_options = nil,
+        timeout_ms = nil,
+      },
+      servers = {
+        gopls = {
+          settings = {
+            gopls = {
+              hints = {
+                assignVariableTypes = false,
+                compositeLiteralFields = false,
+                compositeLiteralTypes = false,
+                constantTypes = false,
+                functionLiteralTypes = false,
+                parameterNames = false,
+                rangeVariableTypes = false,
+                receiverTypes = false,
+                returnVariableTypes = false,
+              },
+            },
+          },
+        },
+      },
+      setup = {}, -- Add an empty setup table
+    }
+    return ret
+  end,
+}
